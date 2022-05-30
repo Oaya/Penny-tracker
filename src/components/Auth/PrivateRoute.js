1b9053686e610e23ce11f30f113c135cdf98e34e
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-29 17:27:08 -0700

    change the private route alternetive

--- src/components/Auth/PrivateRoute.js
+++ src/components/Auth/PrivateRoute.js
@@ -3,13 +3,20 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import { useAuth } from "../../contexts/AuthContext";
-import Login from "./Login";
+import Welcome from "../../pages/Welcome";
 
 const PrivateRoute = ({ component, ...rest }) => {
   const { currentUser } = useAuth();
 
-  const renderingComponent = currentUser ? component : Login;
-  return <Route {...rest} component={renderingComponent} />;
+  const renderingComponent = currentUser
+    ? component
+    : Welcome;
+  return (
+    <Route
+      {...rest}
+      component={renderingComponent}
+    />
+  );
 };
 
 export default PrivateRoute;
